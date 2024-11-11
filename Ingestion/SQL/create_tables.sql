2b06b2eccd7643f3a0feeec018d6245d0c26c390
Subash Ghimire <subash.ghimire@verisk.com>
2024-11-11 13:46:03 +0545

    Adds ingestion scripts

---
+++ Ingestion/SQL/create_tables.sql
@@ -0,0 +1,26 @@
+
+CREATE TABLE IF NOT EXISTS Date_Lookup
+(
+    bs_date STRING,
+    ad_date STRING
+)
+
+CREATE TABLE IF NOT EXISTS "gold_silver"
+(
+    "date" STRING,
+    "fine_gold" STRING,
+    "standard_gold" STRING,
+    "silver" STRING
+)
+
+CREATE TABLE IF NOT EXISTS "forex"
+(
+    "date" STRING,
+    "published_on" STRING,
+    "modified_on" STRING,
+    "buy" STRING,
+    "sell" STRING,
+    "iso3" STRING,
+    "name" STRING,
+    "unit" STRING
+)
